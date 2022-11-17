2a6e8c8be75307d00e7e46ee821631069b670fb4
tinld <19522348@gm.uit.edu.vn>
2022-11-17 11:57:31 +0700

    upload signup

--- controllers/signup.js
+++ controllers/signup.js
@@ -4,7 +4,8 @@ const postUserData = async (req, res) => {
     const Email = req.body.email;
     const Password = req.body.password;
     try{
-        const findUser = await users.findOne({email: Email});
+        const findUser = await users.findOne({email: Email}); // Tìm kiếm trong mongo
+        //->>>> findUser = {email, password} ? {}
         const matchPassword = await bcrypt.compare(Password, findUser.password);
         if(!matchPassword){
             throw new Error('No User Found');
