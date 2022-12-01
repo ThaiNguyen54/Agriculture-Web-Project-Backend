fb561780dc08040b54a903ae9def1cf98c99f0a3
ThanhOwO <91793770+ThanhOwO@users.noreply.github.com>
2022-12-01 21:35:43 +0700

    fix url delete question & del answer

--- app/routers/answer.js
+++ app/routers/answer.js
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post("/ver1/authenticate/answer", AnswerControl.AddAnswer)
 router.get("/ver1/answers", AnswerControl.GetAllAnswer)
 router.get("/ver1/answers/:UserID", AnswerControl.GetAnswerByUserID)
-router.delete("/ver1/answers/:AnswerId", AnswerControl.DeleteAnswer)
+router.delete("/ver1/authenticate/answers/:AnswerId", AnswerControl.DeleteAnswer)
 router.put("/ver1/authenticate/answers/:AnswerId", AnswerControl.UpdateAnswer)
 export default router;
